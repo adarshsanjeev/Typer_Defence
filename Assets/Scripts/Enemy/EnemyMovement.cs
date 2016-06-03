1559217a68792d854faaa94e2648dc63e6660206
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2016-06-03 04:26:35 +0300

    Enemy animation controller

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform MainTower;
+    GameObject tower;
+    //EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+    private Animator myAnimator;
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Awake ()
+    {
+        tower = GameObject.FindGameObjectWithTag("Player");
+        MainTower = GameObject.FindGameObjectWithTag ("Player").transform;
+        
+        //playerHealth = player.GetComponent <PlayerHealth> ();
+        //enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <NavMeshAgent> ();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == tower)
+        {
+            myAnimator.SetBool("reachedTower", true);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == tower)
+        {
+            myAnimator.SetBool("reachedTower", false);
+        }
+    }
+
+    void Update ()
+    {
+        if (myAnimator.GetBool("reachedTower") == false)
+        {
+            nav.SetDestination (MainTower.position);
+        }
+        else if(myAnimator.GetBool("reachedTower") == true)
+        {
+            nav.enabled = false;
+        }
+    }
+}
